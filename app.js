9c54f18f1829a7d8752a09de5f9eadb805f6ab42
amethyste <javiahernandez67@gmail.com>
2025-07-03 12:24:56 -0600

    datos iniciales de libros en formato json

---
+++ app.js
@@ -0,0 +1,8 @@
+// Datos iniciales de libros en formato JSON
+let biblioteca = {
+    "libros": [
+        { "titulo": "Cien años de soledad", "autor": "Gabriel García Márquez", "genero": "Realismo mágico", "disponible": true },
+        { "titulo": "1984", "autor": "George Orwell", "genero": "Distopía", "disponible": true }
+    ]
+};
+
